a64a491d357b2e2d0b8b7cc172ef0bb38ce2d3f2
silvia1241035 <1241035@isep.ipp.pt>
2026-05-28 15:15:24 +0100

    Ficha 10-Código para o ficheiro index.php do private não seja acedido por URL

--- private/index.php
+++ private/index.php
@@ -4,6 +4,23 @@ require_once __DIR__ . '/../config/config.php';
 
 <!DOCTYPE html>
 <html lang="pt">
+    <?php
+    // ---------------------------------------------------------------------------
+    // SEGURANÇA: Impede que o utilizador aceda diretamente a este script.
+    // Este ficheiro deve ser acedido apenas através de submissão de formulário (POST).
+    // Se for acedido diretamente (por URL) recebe a informação de Acesso Inválido
+    // ----------------------------------------------------------------------------
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    // Se não for uma submissão do formulário, termina o script
+    exit('Acesso inválido');
+    }
+    // Mostrar os dados recebidos pelo formulário através do método POST
+    // O nome dos campos (entre aspas) deve ser igual ao atributo "name" no login.php
+    echo "Utilizador: " . $_POST['text_username'] . "<br>"; // Mostra o username inserido
+    echo "Password: " . $_POST['text_password']; // Mostra a password inserida
+    // Em produção, **nunca** se deve mostrar a password assim — isto é apenas para testes!
+    ?>
+
 
     <?php include 'includes/header.php'; ?>
 
--- public/login.php
+++ public/login.php
@@ -19,16 +19,16 @@
 
                     <div class="row"> <!--cria uma linha no layout para as colunas-->
                         <div class="col">
-                            <form action="../backend/index.html" method="post">
+                            <form action="../private/index.php" method="post">
                                 <div class="mb-3">
                                     <!-- Utilizador -->
                                     <label for="email" class="form-label">Utilizador</label>
-                                    <input type="email" name="email" id="email" class="form-control">
+                                    <input type="email" name="text_username" id="">
                                 </div>
                                 <div class="mb-3">
                                     <!-- Password -->
                                     <label for="password" class="form-label">Password</label>
-                                    <input type="password" name="password" id="password" class="form-control">
+                                    <input type="password" name="text_password" id="">
                                 </div>
                                 <div class="mb-3 text-center">
                                     <!-- Submit -->
